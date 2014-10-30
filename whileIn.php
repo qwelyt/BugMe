eda5bbedd288e3bda2f07330e21335855ec3c885
qwelyt <qwelyt@users.noreply.github.com>
2014-10-30 20:18:10 +0100

    Added some comments about  what should be there.

--- whileIn.php
+++ whileIn.php
@@ -1,1 +1,13 @@
 Hello thar <?php echo $_SESSION['usr']; ?>.
+<?php
+// query database for what bugs exists
+// Then display them.
+
+//for number of rows
+//	<div class='bug'>
+//		<span class='bug-title'>$bugTitle</span>
+//		<span class='bug-id'>$bugID</span>
+//		<span class='bug-submitter'>$bugSub</span>
+//		<span class='bug-status'>$bugStats</span>
+//		<div class='bug-desc'>$bugDesc</div>
+//	</div><!-- bug -->

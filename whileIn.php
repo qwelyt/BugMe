9c5f5cf28ea277517183932e029e59dfbf189071
qwelyt <qwelyt@users.noreply.github.com>
2014-10-31 15:45:21 +0100

    Just more comments.

--- whileIn.php
+++ whileIn.php
@@ -3,6 +3,12 @@ Hello thar <?php echo $_SESSION['usr']; ?>.
 // query database for what bugs exists
 // Then display them.
 
+// <div id='bug-search'>
+//		Search for a bug
+//		<span id='bug-search-input'><input type='text' placeholder='Search...'></span>
+//		<span id='bug-search-id'><input type='scrollliste?? ></span>
+// </div>
+// Probably place this in the center of the page.
 //for number of rows
 //	<div class='bug'>
 //		<span class='bug-title'>$bugTitle</span>
